3f73cc7195dd0936a505f36b05aae3665f384c09
theoallen <theoallen@users.noreply.github.com>
2014-09-12 05:30:46 +0700

    Create Prevent TP Charge by Damage.rb

---
+++ Prevent TP Charge by Damage.rb
@@ -0,0 +1,48 @@
+=begin
+ 
+  Theo - Prevent Charge TP by Damage
+  
+  This script prevent some skill damage not charge the actor's TP if actor is
+  getting hit. Simply put <no charge> in skill / item notebox.
+  
+  Terms of Use :
+  If you feel grateful, write TheoAllen in your credit list. Free to use for
+  both non-commercial and commercial
+ 
+=end
+class RPG::UsableItem
+  
+  def no_charge?
+    return @no_charge if @no_charge
+    @no_charge = !note[/<no[\s_]+charge>/i].nil?
+    return @no_charge
+  end
+  
+end
+ 
+class Game_ActionResult
+  attr_accessor :carried_item
+  
+  alias aed_clear clear
+  def clear
+    aed_clear
+    @carried_item = nil
+  end
+  
+  alias aed_make_damage make_damage
+  def make_damage(value, item)
+    @carried_item = item
+    aed_make_damage(value, item)
+  end
+  
+end
+ 
+class Game_Battler
+  
+  alias aed_charge_tp charge_tp_by_damage
+  def charge_tp_by_damage(damage_rate)
+    return if @result.carried_item.no_charge?
+    aed_charge_tp(damage_rate)
+  end
+  
+end
